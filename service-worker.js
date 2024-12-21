cab555d0bb2ffbfc9c64d8a63a7cfc4c22fd629c
Amir <m00h4mm4dweb@gmail.com>
2024-12-21 21:33:34 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("my-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "https://github.com/6mir/index.html",
+        "https://github.com/6mir/src/style/app.css",
+        "https://github.com/6mir/src/js/app.js",
+        "https://github.com/6mir/picture/favicon/img.ico",
+        "https://github.com/6mir/picture/image/img-1.png",
+        // لیست سایر فایل‌هایی که می‌خواهید ذخیره شوند
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
